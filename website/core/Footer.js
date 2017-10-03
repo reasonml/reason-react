af24ae7033c2272b9efb6aec57ec335fbb5c1be9
Ricky Vetter <rickyvetter@fb.com>
2017-10-02 21:11:35 -0700

    [Docs] only apply pjax to nav links

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -29,13 +29,14 @@ class Footer extends React.Component {
       <span>
         <script src={this.props.config.baseUrl + 'js/redirectBlog.js'}></script>
         <script src={this.props.config.baseUrl + 'js/pjax-api.js'}></script>
-        <script dangerouslySetInnerHTML={{__html: `new Pjax({
+        <script dangerouslySetInnerHTML={{__html: `window.foo = new Pjax({
           areas: [
             // try to use the first query.
-            '.mainContainer, .toc .navWrapper',
+            '.mainContainer, .docsNavContainer .toc .navWrapper',
             // fallback
             'body'
-          ]
+          ],
+          link: '.docsNavContainer:not(.docsSliderActive) a'
         });
         var languagesMenuItemCopy = document.getElementById("languages-menu");
         languagesMenuItemCopy.addEventListener("click", function(e){
