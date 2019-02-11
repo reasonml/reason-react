2a5c47f3732758bde1189a2debe6cfca2c4aed98
Ricky Vetter <rickyvetter@fb.com>
2019-02-11 13:24:21 -0500

    better fragment handling

---
+++ jsx_ppx/test.ml
@@ -0,0 +1,21 @@
+[@@@bs.config { jsx = 3 }]
+let handleClick _event =
+  Js.log (("clicked!")[@reason.raw_literal "clicked!"])
+let make ~message  () =
+  React.useEffect
+    (fun ()  -> Js.log (("Hey!")[@reason.raw_literal "Hey!"]); None);
+  ((button ~onClick:handleClick ~children:[ReasonReact.string message] ())
+  [@JSX ])[@@react.component ]
+module A =
+  struct
+    let make ~className  ~children  =
+      ((div ~className ~children:[children] ())[@JSX ])[@@react.component ]
+  end
+module B =
+  struct
+    let make () =
+      ((A.createElement ~className:(("test")[@reason.raw_literal "test"])
+          ~children:[((div ~children:[] ())[@JSX ]);
+                    ((div ~children:[] ())[@JSX ])] ())[@JSX ])[@@react.component
+                                                                 ]
+  end
