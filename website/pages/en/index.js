a6793d6cd22f3292766f60c8ccd38e42096a6c46
Ricky Vetter <rickyvetter@fb.com>
2019-03-29 11:29:09 -0400

    initial docs reorg

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -29,28 +29,19 @@ const code = "`";
 
 // fake, static, responsive refmt, lol. See reason.css homeCodeSnippet logic
 const codeExampleSmallScreen =`${pre}reason
-let component =
-  ReasonReact.statelessComponent("Greeting");
-
-let make = (~name, _children) => {
-  ...component,
-  render: _self =>
+[@react.component]
+let make = (~name) =>
     <button>
-      {ReasonReact.string("Hello!")}
-    </button>
-};
+      {React.string("Hello!")}
+    </button>;
 ${pre}`;
 
 const codeExampleLargeScreen =`${pre}reason
-let component = ReasonReact.statelessComponent("Greeting");
-
-let make = (~name, _children) => {
-  ...component,
-  render: _self =>
+[@react.component]
+let make = (~name) =>
     <button>
-      {ReasonReact.string("Hello!")}
-    </button>
-};
+      {React.string("Hello!")}
+    </button>;
 ${pre}`;
 
 const quickStart = `${pre}bash
