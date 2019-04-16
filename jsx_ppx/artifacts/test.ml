742b93b53a4d49c1ba77ad9fe4d37420582bcc2e
Ricky Vetter <rickyvetter@fb.com>
2019-04-16 10:26:43 -0400

    use empty loc to clear up locations that do exist

---
+++ jsx_ppx/artifacts/test.ml
@@ -0,0 +1,6 @@
+    ocamlopt jsx_ppx/.reactjs_jsx_ppx_v3.eobjs/native/reactjs_jsx_ppx_v3.{cmx,o} (exit 2)
+(cd _build/default && /Users/rickyvetter/.esy/3_____________________________________________________________/i/ocaml-4.2.3007-31049140/bin/ocamlopt.opt -w -40 -w -9 -g -I jsx_ppx/.reactjs_jsx_ppx_v3.eobjs/byte -I jsx_ppx/.reactjs_jsx_ppx_v3.eobjs/native -I /Users/rickyvetter/.esy/3_____________________________________________________________/i/ocaml-4.2.3007-31049140/lib/ocaml/compiler-libs -intf-suffix .ml -no-alias-deps -o jsx_ppx/.reactjs_jsx_ppx_v3.eobjs/native/reactjs_jsx_ppx_v3.cmx -c -impl jsx_ppx/reactjs_jsx_ppx_v3.ml)
+File "jsx_ppx/reactjs_jsx_ppx_v3.ml", line 679, characters 19-27:
+Error: This expression has type Location.t
+       but an expression was expected of type
+         string Asttypes.loc = string Location.loc
