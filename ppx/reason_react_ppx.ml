52e585a2a5bd267a46c925a609bf7b2155a188d8
David Sancho Moreno <dsnxmoreno@gmail.com>
2024-11-25 19:48:42 +0100

    Rollback change on _ppx

--- ppx/reason_react_ppx.ml
+++ ppx/reason_react_ppx.ml
@@ -647,10 +647,8 @@ let jsxMapper =
     let expr = mapper#expression ctxt expr in
     match expr.pexp_desc with
     | Pexp_fun (Labelled "key", _, _, _) | Pexp_fun (Optional "key", _, _, _) ->
-        raise
-          (Invalid_argument
-             "Key cannot be accessed inside of a component. Don't worry - you \
-              can always key a component from its parent!")
+        Location.raise_errorf ~loc:expr.pexp_loc
+          ("~key cannot be accessed from the component props. Please set the key where the component is being used.")
     | Pexp_fun
         ( ((Optional label | Labelled label) as arg),
           default,
