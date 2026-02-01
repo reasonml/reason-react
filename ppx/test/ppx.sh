2d8df21a2e592e5d62a4190dd5a556e6717cd539
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-02-01 13:38:58 -0800

    build: remove circular dependency between reason-react{,-ppx} at test time (#918)
    * build: remove circular dependency between reason-react{,-ppx} at test time
    
    * fix interpreter
    
    * wut
    
    * huh
    
    * wip
    
    * wtf
    
    * bash
    
    * test redirection
    
    * invert

--- ppx/test/ppx.sh
+++ ppx/test/ppx.sh
@@ -1,27 +1,3 @@
-#!/bin/bash
 
-set -eo pipefail
-
-function usage() {
-  echo "Usage: $(basename "$0") --output re [file.re]"
-  echo "       $(basename "$0") --output ml [file.re]"
-}
-
-if [ -z "$3" ]; then
-  usage
-  exit
-fi
-
-refmt --parse re --print ml "$3" > output.ml
-reason-react-ppx --impl output.ml -o temp.ml
-
-if [ "$2" == "ml" ]; then
-  cat temp.ml
-  exit
-elif [ "$2" == "re" ]; then
-  refmt --parse ml --print re temp.ml
-  exit
-else
-  usage
-  exit
-fi
+root_path=$PWD
+bash "$(dirname "$0")/run-ppx.sh" "$@"
